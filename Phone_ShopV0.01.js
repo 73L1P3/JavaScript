f3d493e4368427dda912ea18c29831f825e90fd5
jhesgodi <jhesgonzalez@utp.edu.co>
2019-02-18 23:47:38 -0500

    1st raw revision

--- Phone_ShopV0.01.js
+++ Phone_ShopV0.01.js
@@ -1,44 +1,88 @@
-var Total_Money = prompt("How much money do you have in your bank account?");
-const Tax_Rate = .08;
-var Phone_Price = 250;
-let Accessory_1 = 20;
-let Accessory_2 = 5;
-var Spending_Threshold = 300;
-var Price = 0;
+// Please use only var, or (const + let)
+// It is better use: camelCaseForVariableNames
+// Most javascripter's will use single quotes: '', with the exception of "How are you? 'your name'"
 
-function printAmount(amt){
+// Take into account:
+// 1. You can have base variables like: base price, base taxe, base account balance
+
+
+// const initialAmount = 300
+// let currentAmount = initialAmount // => currentAmount = currentAmount - 100
+
+
+let Total_Money = prompt("How much money do you have in your bank account?"); // totalMoney
+const Tax_Rate = .08;
+const Phone_Price = 250;
+const Spending_Threshold = 300;
+let Accessory_1 = 20; 
+let Accessory_2 = 5;
+
+var Price = 0; // find a better name? maybe? right? ... ok
+
+
+
+function printAmount(amt){ // amt not better than 'amount'
     console.log(amt.toFixed(2));
 }
 
 function formatAmount(){
+    // consider template strings
+    // return `$${Total_Money.toFixed(2)}`
     return "$" + Total_Money.toFixed(2);
 }
 
-function CalculateFinalAmount(amt){
+function CalculateFinalAmount(amt){ // consistency calculateFinalAmount
   amt = amt + (amt * Tax_Rate);
-  return amt;
+  return amt; // return amt + (amt * Tax_Rate)
+}
+
+function getInitialPhonePrice() {
+    alert("We verify your Bank Account and we find enough money to buy this phone!");
+      Phone_Price = CalculateFinalAmount(Phone_Price); // purchasePrice
+      alert(`The final cost of the phone is ${(Phone_Price)}`);
+
+      return Phone_Price
+}
+
+function discountPurchaseAmount (amount) {
+    accountBalance -= amount;
+    accountPurchaseTotal += amount;
 }
 
 
-do {
-   alert("Morning sir,  We have a phone thats currently on Sale! It's only $250 plus Taxes, Are you going to buy it?");
+do { // take care of identation
+  alert("Morning sir,  We have a phone thats currently on Sale! It's only $250 plus Taxes, Are you going to buy it?");
+
   let Buy = prompt ("Yes or No?");
-  
-  if (Buy === "Yes" || Buy === "yes"){
+                                        
+
+  if (Buy === "Yes" || Buy === "yes"){ // buy.toUpperCase() === 'YES'
+
+    // first purchasePrice, the validate
     if(Total_Money >= Phone_Price){
-      alert("We verify your Bank Account and we find enough money to buy this phone!");
-      Phone_Price = CalculateFinalAmount(Phone_Price);
-      alert(`The final cost of the phone is ${(Phone_Price)}`);
-      Total_Money -= Phone_Price;
-      Price = Price + Phone_Price;
+      const purchaseTotal = getInitialPhonePrice()
+      discountPurchaseAmount(purchaseTotal)
+
+
       console.log(`You already spent ${(Price)}`);
       console.log(`The money you have in your bank account is ${Total_Money}`);
       
+      // check treshold here, and if true
       alert('Are you interested in buy an accessory for your phone?');
       let Choose = prompt("Yes or No");
         if (Choose === "Yes" || Choose === "yes"){
           alert('Currently we have 2, a Phone Case "(1)" and a Screen Protector"(2)"');
           let Choose_2 = prompt('What would you like to buy?');
+
+
+          // let selection = prompt('Which one would you like to buy?');
+          // switch(selection) {
+              // case '1': 
+                // break
+              // default:
+              //alert('Oh, Ok... Have a nice day!');
+          // }
+
           if (Choose_2 === '1' || Choose === "phone case" || Choose === "Phone Case"){
             Accessory_1 = CalculateFinalAmount(Accessory_1);
             console.log(`Accessory_1 is ${(Accessory_1)}`);
@@ -66,5 +110,12 @@ do {
   }
   console.log(`Lo que sobra es (ultimo 'Total_Money' console.log) ${Total_Money}`);
   Total_Money -= Total_Money;
-} while (Total_Money = 0);
+} while (Total_Money >= 0);
 
+
+
+// Re name variables (take care of let and const)
+// Use switch instead
+// What about if you make a function to prompt any message?
+// What about a while, instead of 'do while'
+// *Try to reduce if nesting, maybe create some functions to isolate your code
